44db1da95ce58b6b1fbc66ef483f2b95450ee66b
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2023-03-14 10:03:55 +0700

    edit api endpoint

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const path = require('path');
-// const userRoutes = require('./routes/users.js');
-// const postRoutes = require('./routes/posts.js');
-// const commentRoutes = require('./routes/comments.js');
-// const likeRoutes = require('./routes/likes.js');
-// const authRoutes = require('./routes/auth.js');
+
 const router = require('./routes/index');
 const app = express();
 const public = __dirname + "/public/";
@@ -25,12 +21,7 @@ const connect = () => {
 };
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', router);
-// app.use("/api/auth", authRoutes);
-// app.use("/api/users", userRoutes);
-// app.use("/api/posts", postRoutes);
-// app.use("/api/comments", commentRoutes);
-// app.use("/api/likes", likeRoutes);
+app.use('/api/', router);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(public + "index.html"));
